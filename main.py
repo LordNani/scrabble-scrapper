ae216b061c8f196a52c31526e4260babf1597344
LordNani <rigansu@gmail.com>
2021-08-20 20:21:32 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+import lxml.html
+import openpyxl
+
+
+def parse(url):
+    response = requests.get(url)
+    tree = lxml.html.document_fromstring(response.text)
+    words = tree.xpath('/html/body/div[4]/div[1]/div[1]/div[1]/div[2]/ul/li/a/text()')
+    return words
+
+
+def write_to_xls(number, words, wb):
+    page_name = f'Page {number}'
+    wb.create_sheet(title=page_name)
+    sheet = wb[page_name]
+    for word in words:
+        cell = sheet.cell(row=words.index(word) + 1, column=1)
+        cell.value = word
+
+
+def main():
+
+    url = 'https://www.allscrabblewords.com/{number}-letter-words/'
+    wb = openpyxl.Workbook()
+    wb.remove(wb['Sheet'])
+    for i in range(2, 13):
+        content = parse(url.format(number=i))
+        write_to_xls(i, content, wb)
+        # print(content)
+        # print(f'{i = }, {content}')
+
+    wb.save('scrabble.xlsx')
+
+def test():
+    wb = openpyxl.load_workbook('scrabble.xlsx')
+    print(wb.sheetnames)
+    sheet = wb['Page 2']
+    print()
+
+if __name__ == '__main__':
+    test()
